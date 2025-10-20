3526466e59760e98ec601b1071eeb0ed0b7dd289
Udhav Saraswat <87545316+Udhav-Saraswat@users.noreply.github.com>
2025-10-21 02:00:11 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
 };
 
+module.exports = nextConfig;
 export default nextConfig;
